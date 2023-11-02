9ce82552f59a640ea9c858303d9b96aac73d062f
Hosung <an3735297@naver.com>
2023-11-02 19:20:45 +0900

    compoent: 컴포넌트 추출하기

--- component-start/src/components/ExpenseItem.js
+++ component-start/src/components/ExpenseItem.js
@@ -1,25 +1,24 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseInfo(props) {
+  return <div className="expense-item__description">
+    <h2>{props.title}</h2>
+    <div className="expense-item__price">{props.amount}</div>
+  </div>;
+}
 
 function ExpenseItem(props) {
   const expenseDate = new Date(2021, 2, 28)
   const expenseTitle = "Car insurance"
   const expenseAmount = 294.12
 
-  const month = props.date.toLocaleString("en-US", {month: 'long'})
-  const day = props.date.toLocaleString("en-US", {day: '2-digit'})
-  const year = props.date.getFullYear()
-
   return (
       <div className="expense-item">
         <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
+          <ExpenseDate date={expenseDate}/>
         </div>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">{props.amount}</div>
-        </div>
+        <ExpenseInfo title={expenseTitle} amount={expenseAmount}/>
       </div>
   );
 }
