849aba18a02335fbd0a210198889999836d44fbc
Hosung <an3735297@naver.com>
2023-11-02 11:42:33 +0900

    component : component 생성

---
+++ component-start/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div> 
+      <div>
+        <h2>Car insurance</h2>
+        <div>$294.12</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
