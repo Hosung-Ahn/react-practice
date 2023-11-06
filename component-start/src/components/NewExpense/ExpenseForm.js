c49d8f852f625d58487463c821c22331549a299c
Hosung <an3735297@naver.com>
2023-11-06 22:20:59 +0900

    state : 여러개의 state 하나로 관리하기

--- component-start/src/components/NewExpense/ExpenseForm.js
+++ component-start/src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,37 @@ import "./ExpenseForm.css";
 import {useState} from "react";
 
 function ExpenseForm(props) {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    title : "",
+    amount : "",
+    date : ""
+  })
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      title : event.target.value
+    })
   }
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      amount : event.target.value
+    })
   }
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      date : event.target.value
+    })
   }
 
   return (
