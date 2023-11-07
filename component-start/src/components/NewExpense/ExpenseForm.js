a8af09dbe10eb3bf3dcfa74d6d76cd56c30725f0
Hosung <an3735297@naver.com>
2023-11-07 20:42:12 +0900

    state : changeHandler / 하나의 핸들러로 여러 input 연결하기

--- component-start/src/components/NewExpense/ExpenseForm.js
+++ component-start/src/components/NewExpense/ExpenseForm.js
@@ -2,46 +2,57 @@ import "./ExpenseForm.css";
 import {useState} from "react";
 
 function ExpenseForm(props) {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    title : "",
-    amount : "",
-    date : ""
-  })
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   title : "",
+  //   amount : "",
+  //   date : ""
+  // })
+
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   title : event.target.value
     // })
-    setUserInput((prevState) => {
-      return {...prevState, title : event.target.value}
-    })
+    // setUserInput((prevState) => {
+    //   return {...prevState, title : event.target.value}
+    // })
   }
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
+    setEnteredAmount(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   amount : event.target.value
     // })
-    setUserInput((prevState) => {
-      return {...prevState, amount : event.target.value}
-    })
+    // setUserInput((prevState) => {
+    //   return {...prevState, amount : event.target.value}
+    // })
   }
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   date : event.target.value
     // })
-    setUserInput((prevState) => {
-      return {...prevState, date : event.target.value}
-    })
+    // setUserInput((prevState) => {
+    //   return {...prevState, date : event.target.value}
+    // })
+  }
+
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value);
+    } else if (identifier === "amount") {
+      setEnteredAmount(value);
+    } else {
+      setEnteredDate(value);
+    }
   }
 
   return (
@@ -49,11 +60,15 @@ function ExpenseForm(props) {
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input type="text" onChange={(event) => {
+              inputChangeHandler("title", event.target.value)
+            }}/>
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input type="number" min="0.01" step="0.01" onChange={(event) => {
+              inputChangeHandler("amount", event.target.value);
+            }}/>
           </div>
           <div className="new-expense__control">
             <label>Date</label>
@@ -65,6 +80,6 @@ function ExpenseForm(props) {
         </div>
       </form>
   );
-}
+};
 
 export default ExpenseForm;
