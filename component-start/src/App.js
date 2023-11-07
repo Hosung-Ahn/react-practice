ea948ac84b7a08ae4f3d44a26a28fa47e2fa8ffa
Hosung <an3735297@naver.com>
2023-11-07 23:12:34 +0900

    state : ExpenseFilter 컴포넌트 생성

--- component-start/src/App.js
+++ component-start/src/App.js
@@ -1,17 +1,24 @@
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
 
 function App() {
-  const expenses = [
-    {title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28)},
-    {title: "Toilet Paper", amount: 94.12, date: new Date(2021, 5, 12)},
-    {title: "New TV", amount: 799.49, date: new Date(2021, 5, 12)},
-    {title: "New Desk (Wooden)", amount: 450, date: new Date(2021, 5, 12)}
-  ]
+  const [expenses, setExpenses] = useState(
+      [
+        {title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28)},
+        {title: "Toilet Paper", amount: 94.12, date: new Date(2021, 5, 12)},
+        {title: "New TV", amount: 799.49, date: new Date(2021, 5, 12)},
+        {title: "New Desk (Wooden)", amount: 450, date: new Date(2021, 5, 12)}
+      ]
+  )
+
 
   const addExpenseHandler = (expense) => {
     console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    })
+    console.log(expenses);
   }
 
   return (
--- component-start/src/components/expenses/Expenses.js
+++ component-start/src/components/expenses/Expenses.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../ui/Card';
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState("2019");
+  const onFilterYearHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+    console.log(selectedYear);
+  }
   return (
       <Card className="expenses">
+        <ExpensesFilter onFilterYear={onFilterYearHandler}/>
         <ExpenseItem
             title={props.items[0].title}
             amount={props.items[0].amount}
@@ -28,6 +35,7 @@ function Expenses(props) {
             date={props.items[3].date}
         />
       </Card>
+
   );
 }
 
