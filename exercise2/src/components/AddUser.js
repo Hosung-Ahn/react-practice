3ee66f4c4ba338002d15c354874ce3167aabe8b3
Hosung <an3735297@naver.com>
2023-11-18 15:21:57 +0900

    exercise2-practice : ref 를 사용해 사용자 입력값 받기

--- exercise2/src/components/AddUser.js
+++ exercise2/src/components/AddUser.js
@@ -1,8 +1,10 @@
 import styles from "./AddUser.module.css";
 import Button from "./Button";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 function AddUser(props) {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const [enteredValue, setEnteredValue] = useState({
     name: '',
@@ -20,6 +22,8 @@ function AddUser(props) {
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    console.log(nameInputRef.current.value);
+    console.log(ageInputRef.current.value);
     if (enteredValue.name.trim().length === 0 || enteredValue.age.trim().length === 0) {
       props.onInvalidInput("Please enter a valid name and age (non-empty values).");
       return;
@@ -39,11 +43,13 @@ function AddUser(props) {
       <div>
         <form className={styles.input} onSubmit={onSubmitHandler}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" value={enteredValue.name} onChange={(event) => {
+          <input id="username" type="text" value={enteredValue.name} ref={nameInputRef}
+                 onChange={(event) => {
             onChangeHandler("name", event.target.value)
           }}/>
           <label htmlFor="age">Age (Years)</label>
-          <input id="age" type="number" value={enteredValue.age} onChange={(event) => {
+          <input id="age" type="number" value={enteredValue.age} ref={ageInputRef}
+                 onChange={(event) => {
             onChangeHandler("age", event.target.value)
           }}/>
           <Button type="submit" text="Add User"/>
